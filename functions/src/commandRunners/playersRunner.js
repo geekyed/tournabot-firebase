cf09e609e76c09abad95e89ff3d102d252951f12
Ed Weston <edward.weston@newvoicemedia.com>
2019-07-04 09:16:59 +0100

    players runner works

---
+++ functions/src/commandRunners/playersRunner.js
@@ -0,0 +1,29 @@
+const tournament = require('../data/tournament')
+const current = require('../data/current')
+
+const execute = async data => {
+  const tournamentID = await current.get(data.channelID)
+  const myTournament = await tournament.get(tournamentID)
+
+  if (!myTournament) throw new Error(`${tournamentID} tournament does not exist!,`)
+
+  if (!myTournament.players) myTournament.players = []
+
+  data.players.forEach(player => {
+    if (!elementIsInArray(player, myTournament.players)) {
+      myTournament.players.push(player)
+    }
+  })
+
+  await tournament.set(myTournament)
+
+  let messages = ['Current Players']
+  myTournament.players.forEach(player => messages.push(player))
+  return { messages, context: `current tournament ${tournamentID}` }
+}
+
+const elementIsInArray = (myElement, myArray) => {
+  return (myArray.findIndex(el => el === myElement) !== -1)
+}
+
+module.exports = { execute }
